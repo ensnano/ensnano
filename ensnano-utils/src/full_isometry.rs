1fb510d70952f6a77632a6038b4571521fc9da14
Haksell <axel.brisse@gmail.com>
2025-10-20 16:32:46 +0200

    fix typos

--- ensnano-utils/src/full_isometry.rs
+++ ensnano-utils/src/full_isometry.rs
@@ -41,7 +41,7 @@ impl FullIsometry {
         Mat3::from_translation(self.translation) * sym_rot
     }
 
-    pub fn matrix_with_transposed_symetry(self) -> Mat3 {
+    pub fn matrix_with_transposed_symmetry(self) -> Mat3 {
         let mut sym_rot = self.rotation.into_matrix().into_homogeneous();
         sym_rot[0] *= self.symmetry.y;
         sym_rot[1] *= self.symmetry.x;
