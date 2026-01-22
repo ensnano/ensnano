c3a6bb47fed6c7f0c5a38c9d3b5579115a5fc82a
Haksell <axel.brisse@gmail.com>
2026-01-22 09:50:31 +0100

    mod node_title_bar

--- ensnano-gui/src/left_panel/organizer/mod.rs
+++ ensnano-gui/src/left_panel/organizer/mod.rs
@@ -1,9 +1,11 @@
 mod hoverable_container;
 mod icon;
+mod node_title_bar;
 mod theme;
 
 use self::{
     hoverable_container::HoverableContainer,
+    node_title_bar::NodeTitleBar,
     theme::{OrganizerTheme, SelectionType},
 };
 use ensnano_design::{
@@ -17,13 +19,12 @@ use ensnano_state::gui::{
     drag_drop_target::{DragDropTarget, DragIdentifier},
     messages::{OrganizerInternalMessage, OrganizerMessage},
 };
-use ensnano_utils::keyboard_priority::keyboard_priority;
 use iced::{
     Element, Length,
     keyboard::Modifiers,
     widget::{
-        Column, Container, Row, Space, button, column, container, horizontal_space, mouse_area,
-        row, scrollable, text, text_input, tooltip,
+        Column, Container, Space, button, column, container, horizontal_space, row, scrollable,
+        text, tooltip,
     },
 };
 use rand::{Rng as _, rngs::ThreadRng};
@@ -1392,142 +1393,6 @@ impl ElementView {
     fn update_element(&mut self, element: &DesignElement) {
         for e in element.attributes() {
             self.attribute_displayers[e.discriminant() as usize].update_attribute(Some(e.clone()));
-        }
-    }
-}
-
-/// A data structure whose view is a "title bar" for a group or a section
-struct NodeTitleBar {
-    state: GroupState,
-    attribute_displayers: Vec<AttributeDisplayer>,
-    name_input_id: text_input::Id,
-}
-
-impl NodeTitleBar {
-    fn new() -> Self {
-        Self {
-            state: GroupState::Idle,
-            attribute_displayers: vec![
-                AttributeDisplayer::new();
-                DesignElement::all_discriminants().len()
-            ],
-            name_input_id: text_input::Id::unique(),
-        }
-    }
-
-    fn new_section() -> Self {
-        Self {
-            state: GroupState::NotEditable,
-            attribute_displayers: vec![],
-            name_input_id: text_input::Id::unique(),
-        }
-    }
-
-    fn start_editing(&mut self) {
-        log::info!("reached view");
-        self.state = GroupState::Editing;
-    }
-
-    fn stop_editing(&mut self) {
-        self.state = GroupState::Idle;
-    }
-
-    fn view(
-        &self,
-        theme: &OrganizerTheme,
-        is_selected: SelectionType,
-        name: &String,
-        id: OrganizerNodeId,
-        expanded: bool,
-    ) -> Element<'_, OrganizerMessage> {
-        let title_row = match &self.state {
-            GroupState::Idle => {
-                let mut row: Row<'_, _> = row![
-                    button(icon::expand_icon(expanded))
-                        .on_press(OrganizerMessage::expand(id.clone(), !expanded)),
-                    Space::with_width(5.0),
-                    mouse_area(
-                        text_input("New group name...", name).id(self.name_input_id.clone())
-                    )
-                    .on_press(OrganizerMessage::edit(id.clone())),
-                    horizontal_space(),
-                    button(icon::plus_icon())
-                        .on_press(OrganizerMessage::add_selection_to_group(id.clone())), // TODO: change icon later !!!
-                    button(icon::edit_icon()).on_press(OrganizerMessage::edit(id.clone())),
-                ];
-
-                for ad in &self.attribute_displayers {
-                    if let Some(view) = ad.view() {
-                        let id = id.clone();
-                        row = row.push(
-                            view.map(move |m| OrganizerMessage::attribute_selected(m, id.clone())),
-                        );
-                    }
-                }
-
-                row = row.push(
-                    button(icon::icon(icondata::BsTrash))
-                        .on_press(OrganizerMessage::delete(id.clone())),
-                );
-                row
-            }
-            GroupState::Editing => {
-                let mut row = row![
-                    button(icon::expand_icon(expanded))
-                        .on_press(OrganizerMessage::expand(id.clone(), !expanded)),
-                    Space::with_width(5.0),
-                    keyboard_priority(
-                        "New group name...",
-                        OrganizerMessage::SetKeyboardPriority,
-                        text_input("New group name...", name)
-                            .id(self.name_input_id.clone())
-                            .on_input(|s| { OrganizerMessage::name_input(s) })
-                            .on_submit(OrganizerMessage::stop_edit())
-                    ),
-                    horizontal_space(),
-                    button(icon::plus_icon())
-                        .on_press(OrganizerMessage::add_selection_to_group(id.clone())), // TODO: change icon later !!!
-                    button(icon::edit_icon()).on_press(OrganizerMessage::stop_edit()),
-                ];
-                for ad in &self.attribute_displayers {
-                    if let Some(view) = ad.view() {
-                        let id = id.clone();
-                        row = row.push(
-                            view.map(move |m| OrganizerMessage::attribute_selected(m, id.clone())),
-                        );
-                    }
-                }
-                row = row.push(
-                    button(icon::icon(icondata::BsTrash))
-                        .on_press(OrganizerMessage::delete(id.clone())),
-                );
-                row
-            }
-            GroupState::NotEditable => row![
-                button(icon::expand_icon(expanded))
-                    .on_press(OrganizerMessage::expand(id.clone(), !expanded)),
-                Space::with_width(5.0),
-                text(name),
-            ],
-        };
-
-        let title_button = button(title_row)
-            .on_press(OrganizerMessage::node_selected(id.clone()))
-            .width(Length::Fill)
-            .style(theme.selected(is_selected));
-        let title_button = HoverableContainer::new(title_button)
-            .on_hover(OrganizerMessage::node_hovered(id.clone(), true))
-            .on_unhover(OrganizerMessage::node_hovered(id.clone(), false));
-        let title_button = DragDropTarget::new(
-            container(title_button).width(Length::Fill),
-            DragIdentifier::Group { id },
-        );
-        container(title_button).into()
-    }
-
-    fn update_attributes(&mut self, attributes: &[Option<DnaAttribute>]) {
-        for (i, a) in attributes.iter().enumerate() {
-            self.attribute_displayers[i].update_attribute(a.clone());
         }
     }
 }
