a190a18eaea1a72b366256bbe03a8199b9174e01
Demonagon <you@example.com>
2026-01-23 11:16:01 +0100

    .

--- ensnano-upcoming/src/lib.rs
+++ ensnano-upcoming/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn exclusive_access() {
+pub fn exclusive_method() {
     panic!(
         "The ensnano-upcoming exclusive depot is not properly configured.\
         Either clone the ensnano-upcoming repository to replace the current ensnano-upcoming folder, or disable the ensnano-upcoming feature."
